eec20acbf0853a90e660918c05a486f1f4deddcb
Craig Rasmussen <rasmus@uoregon.edu>
2017-05-07 15:28:52 -0700

    Cleaned up test_bafio.c and added to Makefile.am as an official test.  WARNING: no test.baf file added yet.

--- aterm/test/test_bafio.c
+++ aterm/test/test_bafio.c
@@ -8,9 +8,7 @@ int main(int argc, char **argv){
 
    ATinit(argc, argv, &bottomOfStack);
   	
-   printf("Hellno!\n");
-  	
-   file = fopen(SRCDIR "/test/" "test.baf", "rb");
+   file = fopen("test.baf", "rb");
    if (!file) {
       printf("ERROR: in test_bafio, path is %s\n", SRCDIR "/test/" "test.baf");
       ATerror("cannot open file \"test.baf\"");
@@ -19,25 +17,5 @@ int main(int argc, char **argv){
    program = ATreadFromBinaryFile(file);
    /*printf("program term is:\n%s\n\n", ATwriteToString(program));*/
 
-#if 0
-   testIntegerEncoding();
-   printf("\n");
-  	
-   testDoubleEncoding();
-   printf("\n");
-  	
-   testWriting();
-   printf("\n");
-  	
-   testReading();
-   printf("\n");
-  	
-   testChunkification();
-   printf("\n");
-	
-   testDeepNesting();
-   printf("\n");
-#endif
-	
    return 0;
 }
